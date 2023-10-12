3f71ee63227f4f817413cfc5102f1e1b100195ef
AlexTheGreat <alexandrewilbur@icloud.com>
2023-10-12 14:33:15 -0400

    Commit #2 October 12 2023

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 # Créé en septembre 2023 par Alexandre Wilbur
 # choisi un chiffre au hasard entre des valeurs définies par l'utilisateur et lui demande de le deviner
+from random import *
 
-from random import*
 
 # déclaration de la fonction qui vérifiera l'essai
+
 def deviner(inconnu):
     essai = -1
     nmbessais = 0
     while essai != inconnu:
         essai = int(input("Entrez un chiffre entre les deux bornes:"))
+        nmbessais = nmbessais + 1
         if essai > inconnu:
-            nmbessais = nmbessais + 1
             print("Votre nombre est plus grand que l'inconnu\n")
         elif essai < inconnu:
-            nmbessais = nmbessais + 1
             print("Votre nombre est plus petit que l'inconnu\n")
         elif essai == inconnu:
-            nmbessais = nmbessais + 1
             print("Bravo! Votre score est : ", nmbessais, "\n")
 
-deviner(randint(int(input("C'est quoi la borne minimale?")), int(input("C'est quoi le borne maximale?"))))
-#Demande pour recommancer le jeu
+
+# Demande pour recommancer le jeu
 retry = "Oui"
 while retry == "Oui":
+    a = int(input("C'est quoi la borne minimale?"))
+    b = int(input("C'est quoi le borne maximale?"))
+    input_inconnu = randint(a, b)
+    deviner(input_inconnu)
     retry = str(input("Voulez-vous rejouer? (Oui ou Non)"))
-    if retry == "Oui": deviner(randint(int(input("C'est quoi la borne minimale?")), int(input("C'est quoi le borne maximale?"))))
